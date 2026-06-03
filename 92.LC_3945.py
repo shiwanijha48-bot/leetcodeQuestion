0a7ed1d2c94c49bba128d31fb588b36dbe0b7496
Shiwani Jha <shiwanijha48@gmail.com>
2026-06-03 23:43:43 +0530

    Implement digit frequency score calculation
    3945. Digit Frequency Score
    Solved
    Easy
    premium lock icon
    Companies
    Hint
    You are given an integer n.
    
    The score of n is defined as the sum of d * freq(d) over all distinct digits d, where freq(d) denotes the number of times the digit d appears in n.
    
    Return an integer denoting the score of n.
    
     
    
    Example 1:
    
    Input: n = 122
    
    Output: 5
    
    Explanation:
    
    The digit 1 appears 1 time, contributing 1 * 1 = 1.
    The digit 2 appears 2 times, contributing 2 * 2 = 4.
    Thus, the score of n is 1 + 4 = 5.
    Example 2:
    
    Input: n = 101
    
    Output: 2
    
    Explanation:
    
    The digit 0 appears 1 time, contributing 0 * 1 = 0.
    The digit 1 appears 2 times, contributing 1 * 2 = 2.
    Thus, the score of n is 2.
     
    
    Constraints:
    
    1 <= n <= 109

---
+++ 92.LC_3945.py
@@ -0,0 +1,13 @@
+class Solution:
+    def digitFrequencyScore(self, n: int) -> int:
+        s =str(n)
+        freq = {}
+        res = 0
+        for i in s:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+        for i in freq:
+            res += int(i)* freq[i]
+        return res
