9e1403883aca156b431ab611527d9b4501331507
Shiwani Jha <shiwanijha48@gmail.com>
2026-07-19 22:48:41 +0530

    Add reverseList function for linked list reversal
    Implement a function to reverse a singly linked list using a stack.

---
+++ 141.LC_206.py
@@ -0,0 +1,54 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = head
+        stack = []
+        while temp is not None:
+            stack.append(temp.val)
+            temp = temp.next
+        temp = head
+        while temp != None:
+            e = stack.pop()
+            temp.val = e
+            temp= temp.next
+        return head
+
+'''
+206. Reverse Linked List
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+'''
