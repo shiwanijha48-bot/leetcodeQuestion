b3e9955f0b6b52236d92c030f026f378ccb76ae3
Shiwani Jha <shiwanijha48@gmail.com>
2026-07-23 21:29:32 +0530

    Implement isPowerOfTwo method in Solution class

---
+++ 146.LC_231.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        elif(n&(n-1))==0:
+            return True
+        else:
+            return False
+        
