c19c97848b72021bd0ae103bab1afeeab4aa9ecc
Shiwani Jha <shiwanijha48@gmail.com>
2026-09-01 23:09:57 +0530

    Add maximumLengthSubstring method for substring problem
    Implement a solution to find the maximum length of a substring with at most two occurrences of each character.

---
+++ 191.LC_3090.py
@@ -0,0 +1,51 @@
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        res = []
+        freq = {}
+        ans = 0
+        for i in s:
+            res.append(i)
+            freq[i] = freq.get(i,0)+1
+            while freq[i] > 2:
+                x = res.pop(0)
+                freq[x] -= 1
+            ans = max(ans, len(res))
+        return ans
+
+
+'''
+3090. Maximum Length Substring With Two Occurrences
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given a string s, return the maximum length of a substring such that it contains at most two occurrences of each character.
+ 
+
+Example 1:
+
+Input: s = "bcbbbcba"
+
+Output: 4
+
+Explanation:
+
+The following substring has a length of 4 and contains at most two occurrences of each character: "bcbbbcba".
+Example 2:
+
+Input: s = "aaaa"
+
+Output: 2
+
+Explanation:
+
+The following substring has a length of 2 and contains at most two occurrences of each character: "aaaa".
+ 
+
+Constraints:
+
+2 <= s.length <= 100
+s consists only of lowercase English letters.
+'''
