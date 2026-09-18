4d042354cb3fdd41df4c2a31a10d08b6c6d97544
Shiwani Jha <shiwanijha48@gmail.com>
2026-09-18 23:30:01 +0530

    Add reverseStr function for string manipulation
    Implement a solution to reverse the first k characters for every 2k characters in a string.

---
+++ 233.LC_541.py
@@ -0,0 +1,44 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        s = list(s)
+        n = len(s)
+        for i in range(0, n, 2*k): # Process every 2k block
+            l = i
+            r = min(i+k-1 , n-1)
+            while l < r:  # Reverse first k characters
+                s[l], s[r] = s[r], s[l]
+                l += 1
+                r -= 1
+        return "".join(s)
+
+# r should be the last index of the first k characters, but it must not go outside the string.
+
+'''
+541. Reverse String II
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+
+If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+
+ 
+
+Example 1:
+
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+Example 2:
+
+Input: s = "abcd", k = 2
+Output: "bacd"
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only lowercase English letters.
+1 <= k <= 104
+'''
