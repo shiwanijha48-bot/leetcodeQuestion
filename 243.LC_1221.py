db496a8d1ccfec681baf09db16a418834aae43c1
Shiwani Jha <shiwanijha48@gmail.com>
2026-09-24 19:18:35 +0530

    Add balancedStringSplit method to Solution class
    Implement a method to count maximum balanced substrings of 'L' and 'R'.

---
+++ 243.LC_1221.py
@@ -0,0 +1,54 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        x= 0
+        count = 0
+        for i in s:
+            if i == 'L':
+                x += 1
+            else:
+                x -= 1
+            if x == 0:
+                count += 1
+        return count
+
+  '''
+1221. Split a String in Balanced Strings
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s, split it into some number of substrings such that:
+
+Each substring is balanced.
+Return the maximum number of balanced strings you can obtain.
+
+ 
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", each substring contains same number of 'L' and 'R'.
+Note that s cannot be split into "RL", "RR", "RL", "LR", "LL", because the 2nd and 5th substrings are not balanced.
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+ 
+
+Constraints:
+
+2 <= s.length <= 1000
+s[i] is either 'L' or 'R'.
+s is a balanced string.
+  '''
